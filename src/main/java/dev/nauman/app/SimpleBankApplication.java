ec86c977164162f3e92fd9cb5b3f55e6bb81828e
mnauman319 <mnauman319@gmail.com>
2020-10-12 10:13:29 -0500

    Final changes to bank app, hopefully

--- src/main/java/dev/nauman/app/SimpleBankApplication.java
+++ src/main/java/dev/nauman/app/SimpleBankApplication.java
@@ -19,6 +19,7 @@ import dev.nauman.services.CustomerServiceImpl;
 @ComponentScan("dev.nauman")
 @EnableJpaRepositories("dev.nauman.repositories")
 public class SimpleBankApplication {
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SimpleBankApplication.class, args);
 	}
@@ -29,11 +30,11 @@ public class SimpleBankApplication {
 			 cserve.createCustomer(new Customer(0,"Dan","password"));
 			 cserve.createCustomer(new Customer(0,"Mike","password"));
 			 cserve.createCustomer(new Customer(0,"Rogger","password",true));
-			aserve.createAccount(new Account(0, 1, "My Savings", 14000));
-			aserve.createAccount(new Account(0, 1, "My Checking", 140));
-			aserve.createAccount(new Account(0, 2, "My Savings", 21000));
-			aserve.createAccount(new Account(0, 2, "My Checking", 2400));
-			aserve.createAccount(new Account(0, 2, "My Vacaction Fund", 10000));
+			 aserve.createAccount(new Account(0, 1, "My Savings", 14000));
+			 aserve.createAccount(new Account(0, 1, "My Checking", 140));
+			 aserve.createAccount(new Account(0, 2, "My Savings", 21000));
+			 aserve.createAccount(new Account(0, 2, "My Checking", 2400));
+			 aserve.createAccount(new Account(0, 2, "My Vacaction Fund", 10000));
 		};
 	}
 	
